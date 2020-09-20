895446789766c37d8a7a7f0f8dcb313a05f89fa9
Anton Mordik <amordik@elinext.com>
2020-09-20 21:58:59 +0300

    added monorepo starter pack

---
+++ scripts/link-packages.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const { resolve } = require('path');
+const { exec } = require("child_process");
+const deps = require('../deps.json');
+const currentDir = process.cwd();
+
+const targetPath = resolve(currentDir, deps.target);
+
+for (const path of deps.paths) {
+  let dirsPath;
+  if (path.endsWith('*')) {
+    dirsPath = resolve(currentDir, path.substring(0, path.length - 1));  
+  } else {
+    dirsPath = resolve(currentDir, path);
+  }
+  const nestedPaths = fs.readdirSync(dirsPath);
+  for (const nested of nestedPaths) {
+    const resultedPath = resolve(dirsPath, nested);
+    exec(`ln -s ${targetPath} ${resultedPath}`, (_, stdout, stderr) => {
+      console.log(stdout, stderr);
+    });
+  }
+}
