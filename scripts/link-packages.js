f3b8d6220b1911d02a0954b797d499c2e6af021a
Anton Mordik <amordik@elinext.com>
2020-09-23 23:54:42 +0300

    template app created

--- scripts/link-packages.js
+++ scripts/link-packages.js
@@ -10,13 +10,16 @@ for (const path of deps.paths) {
   let dirsPath;
   if (path.endsWith('*')) {
     dirsPath = resolve(currentDir, path.substring(0, path.length - 1));  
+    const nestedPaths = fs.readdirSync(dirsPath);
+    for (const nested of nestedPaths) {
+      const resultedPath = resolve(dirsPath, nested);
+      exec(`ln -s ${targetPath} ${resultedPath}`, (_, stdout, stderr) => {
+        console.log(stdout, stderr);
+      });
+    }
   } else {
     dirsPath = resolve(currentDir, path);
-  }
-  const nestedPaths = fs.readdirSync(dirsPath);
-  for (const nested of nestedPaths) {
-    const resultedPath = resolve(dirsPath, nested);
-    exec(`ln -s ${targetPath} ${resultedPath}`, (_, stdout, stderr) => {
+    exec(`ln -s ${targetPath} ${dirsPath}`, (_, stdout, stderr) => {
       console.log(stdout, stderr);
     });
   }
